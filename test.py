cd3b233fc42fad43953ec5fc45fa3c2d0caba432
jeyachandranj <125851197+jeyachandranj@users.noreply.github.com>
2024-02-13 16:01:53 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+number1 = float(input("Enter the first number: "))
+result1 = number1 * 2
+print("Result 1:", result1)
+
+number2 = float(input("Enter the second number: "))
+result2 = number2 * 2
+print("Result 2:", result2)
+
+number3 = float(input("Enter the third number: "))
+result3 = number3 * 2
+print("Result 3:", result3)
